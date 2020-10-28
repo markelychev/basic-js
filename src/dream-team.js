8c8923c02c272891430f8f04a640ca93a655ec9e
A.Markelychev <a.markelychev@gmail.com>
2020-10-28 22:21:12 +0300

    feat: add createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !members.length) return false;
+  return members
+    .filter((member) => typeof member == "string")
+    .map((member) => member.replace(/\s/g, "").slice(0, 1).toUpperCase())
+    .sort()
+    .join("");
 };
