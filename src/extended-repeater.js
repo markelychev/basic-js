78f8adea29aba2c9711380eeecd9ecb8d9870f0e
A.Markelychev <a.markelychev@gmail.com>
2020-10-29 01:42:13 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports =
+function repeater(str, options) {
+  let res = "";
+  const s = str + "";
+  let repeatTimes = options.repeatTimes || 0;
+  const separator = options.separator || "+";
+  let addition = options.addition + "";
+  const additionRepeatTimes = options.additionRepeatTimes || 0;
+  const additionSeparator = options.additionSeparator || "|";
+  for (repeatTimes; repeatTimes > 0; repeatTimes--) {
+    res += s;
+    for (let i = additionRepeatTimes; i > 0; i--) {
+      res += addition;
+      if (i > 1) {
+        res += additionSeparator;
+      }
+    }
+    if (repeatTimes > 1) {
+      res += separator;
+    }
+  }
+  return res || res + s + addition;
+}
