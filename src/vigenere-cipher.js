8df358deaaaa659a93743fdb95c8d35742e5fa5c
A.Markelychev <a.markelychev@gmail.com>
2020-11-01 01:27:13 +0300

    feat: add vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,63 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
+    this.codeA = 65;
+    this.charsLength = 26; //en
+  }
+  encrypt(sourceMessage, key) {
+    if (!(sourceMessage && key)) throw new Error();
+
+    const keyLength = key.length;
+    const self = this;
+    let i = 0;
+    sourceMessage = sourceMessage.toUpperCase();
+    key = key.toUpperCase().split("");
+
+    let res = sourceMessage.split("").map((char) => {
+      if (!/[A-Z]/.test(char)) return char;
+
+      let charCode = char.charCodeAt() - self.codeA;
+      let keyCharCode = key[i % keyLength].charCodeAt() - self.codeA;
+      i++;
+
+      return String.fromCharCode(
+        self.codeA + ((charCode + keyCharCode) % self.charsLength)
+      );
+    });
+
+    return this.mode ? res.join("") : res.reverse().join("");
+  }
+  decrypt(encryptedMessage, key) {
+    if (!(encryptedMessage && key)) throw new Error("Needs two arguments");
+
+    const keyLength = key.length;
+    const self = this;
+    let res = "";
+    let i = 0;
+    key = key.toUpperCase().split("");
+
+    res = encryptedMessage.split("").map((char) => {
+      if (!/[A-Z]/.test(char)) return char;
+
+      let charCode = char.charCodeAt() - self.codeA;
+      let keyCharCode = key[i % keyLength].charCodeAt() - self.codeA;
+      i++;
+
+      return String.fromCharCode(
+        self.codeA +
+          ((charCode - keyCharCode + self.charsLength) % self.charsLength)
+      );
+    });
+    return this.mode ? res.join("") : res.reverse().join("");
   }
 }
 
 module.exports = VigenereCipheringMachine;
+
+// const directMachine = new VigenereCipheringMachine();
+// console.log(
+//   directMachine.encrypt("attack at dawn!", "alphonse"),
+//   "AEIHQX SX DLLU!"
+// );
