66547f08f43d079c0fb20f38f25fac5b25fbca8f
A.Markelychev <a.markelychev@gmail.com>
2021-03-08 01:41:41 +0300

    fix: Extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,17 +2,22 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports =
 function repeater(str, options) {
+  if (str == 'STRING_OR_DEFAULT'){
+    console.log(typeof (str) );
+  }
   let res = "";
-  const s = str + "";
-  let repeatTimes = options.repeatTimes || 0;
+  const s = String(str);
+  let repeatTimes = options.repeatTimes || 1;
   const separator = options.separator || "+";
-  let addition = options.addition + "";
-  const additionRepeatTimes = options.additionRepeatTimes || 0;
+  let addition = options.addition !== undefined ? String(options.addition) : "";
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
   const additionSeparator = options.additionSeparator || "|";
+  
   for (repeatTimes; repeatTimes > 0; repeatTimes--) {
     res += s;
     for (let i = additionRepeatTimes; i > 0; i--) {
       res += addition;
+      
       if (i > 1) {
         res += additionSeparator;
       }
@@ -21,5 +26,7 @@ function repeater(str, options) {
       res += separator;
     }
   }
+  
+  
   return res || res + s + addition;
 }
